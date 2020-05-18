e3174b0ac1c37d601f17943fdb372a2cabe9fe2f
steady-mongoose <31807580+steady-mongoose@users.noreply.github.com>
2020-05-18 12:37:51 -0700

    Add files via upload

---
+++ Office365/Add-Admin-Users-To-Office-365-User-Mailboxes.ps1
@@ -0,0 +1,16 @@
+﻿# Create Office 365 Security Group
+# - https://docs.microsoft.com/en-us/powershell/module/msonline/new-msolgroup?view=azureadps-1.0
+New-MsolGroup -DisplayName "Tenant_Admins" -Description "Admin Group for Office 365"
+
+# Add admin accounts to group
+# - https://www.adamfowlerit.com/2015/08/adding-multiple-cloud-users-to-an-azureoffice-365-security-group/
+$group = get-msolgroup -All | where {$_.Displayname -eq “Tenant_Admins”}
+$users = get-msoluser -All | select userprincipalname,objectid | where {$_.userprincipalname -like “*admin*”}
+$users | foreach {add-msolgroupmember -groupobjectid $group.objectid -groupmembertype “user” -GroupMemberObjectId $_.objectid}
+get-msolgroupmember -groupobjectid $group.objectid
+
+# Add security group to have access to all mailboxes
+# - https://www.quadrotech-it.com/blog/grant-full-access-to-all-mailboxes-in-office-365/
+Get-Mailbox -ResultSize unlimited -Filter {(RecipientTypeDetails -eq 'UserMailbox')} | 
+Add-MailboxPermission -User Tenant_Admins -AccessRights FullAccess -InheritanceType all
+
---
+++ Office365/Connect-To-Office-365.ps1
@@ -0,0 +1,8 @@
+﻿    $credential = Get-Credential
+    if ($cred -eq $null) {$cred = Get-Credential $credential}
+    Write-Output "Connecting to Office 365..."
+    Connect-MsolService -Credential $cred
+    Set-ExecutionPolicy 'RemoteSigned' -Scope Process -Confirm:$false
+    Set-ExecutionPolicy 'RemoteSigned' -Scope CurrentUser -Confirm:$false
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid -Credential $Credential -Authentication Basic -AllowRedirection
+    Import-PSSession $Session -AllowClobber -DisableNameChecking
